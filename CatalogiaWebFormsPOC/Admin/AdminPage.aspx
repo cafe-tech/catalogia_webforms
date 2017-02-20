209680dc2b6121ea735c22222fac779dc90d0c27
Michael Francisco <kiko.peace.mk@gmail.com>
2017-02-21 02:32:31 +0800

    Add files via upload

---
+++ CatalogiaWebFormsPOC/Admin/AdminPage.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminPage.aspx.cs" Inherits="CatalogiaWebForms.Admin.AdminPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Item Catalogue Maintenance</h1>
+    <hr />
+    <h3>Add an Item:</h3>
+    <table>        
+        <tr>
+            <td style="width: 143px"><asp:Label ID="LabelAddCategory" runat="server">Category:</asp:Label></td>
+            <td>
+                <asp:DropDownList ID="DropDownAddCategory" runat="server" 
+                    ItemType="CatalogiaWebForms.Models.CatalogObjectCategory" 
+                    SelectMethod="GetCategories" DataTextField="CategoryName" 
+                    DataValueField="CategoryID" >
+                </asp:DropDownList>
+            </td>
+        </tr>        
+        <tr>
+            <td style="width: 143px"><asp:Label ID="LabelAddName" runat="server">Name:</asp:Label></td>
+            <td>
+                <asp:TextBox ID="AddObjectName" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" Text="* Object name required." ControlToValidate="AddObjectName" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 143px"><asp:Label ID="LabelAddDescription" runat="server">Description:</asp:Label></td>
+            <td>
+                <asp:TextBox ID="AddItemDescription" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" Text="* Description required." ControlToValidate="AddItemDescription" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 143px"><asp:Label ID="LabelAddPrice" runat="server">Price:</asp:Label></td>
+            <td>
+                <asp:TextBox ID="AddCatalogObjectPrice" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" Text="* Price required." ControlToValidate="AddCatalogObjectPrice" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" Text="* Must be a valid price without $." ControlToValidate="AddCatalogObjectPrice" SetFocusOnError="True" Display="Dynamic" ValidationExpression="^[0-9]*(\.)?[0-9]?[0-9]?$"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 143px"><asp:Label ID="LabelAddImageFile" runat="server">Image File:</asp:Label></td>
+            <td>
+                <asp:FileUpload ID="ObjectImage" runat="server" />
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" Text="* Image path required." ControlToValidate="ObjectImage" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+    </table>
+    <p></p>
+    <p></p>
+    <asp:Button ID="btnAddCatalogItem" runat="server" Text="Add Item" OnClick="btnAddCatalogItem_Click" CausesValidation="true"/>
+    <asp:Label ID="LabelAddStatus" runat="server" Text=""></asp:Label>
+    <p></p>
+    <h3>Remove Catalog Object:</h3>
+    <table>
+        <tr>
+            <td><asp:Label ID="lblRemoveCatalogObject" runat="server">Catalog Object:</asp:Label></td>
+            <td><asp:DropDownList ID="ddlRemoveCatalogObject" runat="server" ItemType="CatalogiaWebForms.Models.CatalogObject"
+                    SelectMethod="GetCatalogObjects" AppendDataBoundItems="true" 
+                    DataTextField="ObjectName" DataValueField="ObjectId" >
+                </asp:DropDownList>
+            </td>
+        </tr>
+    </table>
+    <p></p>
+    <asp:Button ID="btnRemoveCatalogObject" runat="server" Text="Remove Catalog Item" OnClick="btnRemoveCatalogItem_Click" CausesValidation="false"/>
+    <asp:Label ID="LabelRemoveStatus" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ CatalogiaWebFormsPOC/Admin/AdminPage.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CatalogiaWebForms.Models;
+using CatalogiaWebForms.Logic;
+
+namespace CatalogiaWebForms.Admin
+{
+    public partial class AdminPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string catalogObjectAction = Request.QueryString["CatalogObjectAction"];
+            if (catalogObjectAction == "add")
+            {
+                LabelAddStatus.Text = "Object added!";
+            }
+
+            if (catalogObjectAction == "remove")
+            {
+                LabelRemoveStatus.Text = "Object removed!";
+            }
+        }
+
+
+        protected void btnAddCatalogItem_Click(object sender, EventArgs e)
+        {
+            Boolean fileOK = false;
+            String path = Server.MapPath("~/Catalog/Images/");
+            if (ObjectImage.HasFile)
+            {
+                String fileExtension = System.IO.Path.GetExtension(ObjectImage.FileName).ToLower();
+                String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
+                for (int i = 0; i < allowedExtensions.Length; i++)
+                {
+                    if (fileExtension == allowedExtensions[i])
+                    {
+                        fileOK = true;
+                    }
+                }
+            }
+
+            if (fileOK)
+            {
+                try
+                {
+                    // Save to Images folder.
+                    ObjectImage.PostedFile.SaveAs(path + ObjectImage.FileName);
+                    // Save to Images/Thumbs folder.
+                    ObjectImage.PostedFile.SaveAs(path + "Thumbs/" + ObjectImage.FileName);
+                }
+                catch (Exception ex)
+                {
+                    LabelAddStatus.Text = ex.Message;
+                }
+
+                // Add the item to the Catalog Table.
+                AddObjects catalogObjects = new AddObjects();
+                bool addSuccess = catalogObjects.AddObject(AddObjectName.Text, AddItemDescription.Text,
+                    AddCatalogObjectPrice.Text, DropDownAddCategory.SelectedValue, ObjectImage.FileName);
+                if (addSuccess)
+                {
+                    // Reload the page.
+                    string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                    Response.Redirect(pageUrl + "?CatalogObjectAction=add");
+                }
+                else
+                {
+                    LabelAddStatus.Text = "Unable to add new item to database.";
+                }
+            }
+            else
+            {
+                LabelAddStatus.Text = "Unable to accept file type.";
+            }
+        }
+
+        
+        public IQueryable GetCategories()
+        {
+            var _db = new CatalogiaWebForms.Models.CatalogObjectContext();
+            IQueryable query = _db.CatalogObjectCategories;
+
+            List<CatalogObjectCategory> cList = new ObservableCollection<Models.CatalogObjectCategory>((IQueryable<CatalogObjectCategory>)query).ToList();
+
+            return query;
+        }        
+
+        public IQueryable GetCatalogObjects()
+        {
+            var _db = new CatalogiaWebForms.Models.CatalogObjectContext();
+            IQueryable query = _db.CatalogObjects;
+            return query;
+        }
+
+        protected void btnRemoveCatalogItem_Click(object sender, EventArgs e)
+        {
+            using (var _db = new CatalogiaWebForms.Models.CatalogObjectContext())
+            {
+                string objectId = ddlRemoveCatalogObject.SelectedValue;
+                var myItem = (from c in _db.CatalogObjects where c.ObjectId == objectId select c).FirstOrDefault();
+                if (myItem != null)
+                {
+                    _db.CatalogObjects.Remove(myItem);
+                    _db.SaveChanges();
+
+                    // Reload the page.
+                    string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                    Response.Redirect(pageUrl + "?CatalogObjectAction=remove");
+                }
+                else
+                {
+                    LabelRemoveStatus.Text = "Unable to locate catalog item.";
+                }
+            }
+        }
+    }
+}
